cd44c886ab94e2035cb12f2f1e8c80272de8a1cd
emlivo1 <48747184+emlivo1@users.noreply.github.com>
2021-06-10 22:57:46 +0200

    implementirana forma za registraciju

---
+++ src/main/java/nrs20/skladiste/RegistracijaController.java
@@ -0,0 +1,26 @@
+package nrs20.skladiste;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class RegistracijaController {
+    public Hyperlink hlLogin;
+    public TextField tfUserName;
+    public TextField tfFullName;
+    public PasswordField pfUserPassword;
+    public PasswordField pfReUserPassword;
+    public Button btnSignUp;
+
+    public void pfKeyTyped(KeyEvent keyEvent) {
+    }
+
+    public void btnRegistration(ActionEvent actionEvent) {
+    }
+
+    public void hlLogin(ActionEvent actionEvent) {
+    }
+}
